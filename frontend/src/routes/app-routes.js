86674c1a68bdc71467b85c2e258d9ebfca0b8416
Iresh Eranga <iresheranga2020al@gmail.com>
2024-07-19 15:02:28 +0530

    Update app-routes.js

--- frontend/src/routes/app-routes.js
+++ frontend/src/routes/app-routes.js
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProductList from '../components/ProductList';
+//import ProductList from '../components/ProductList';
 import OrderList from '../components/OrderList';
+import { Product } from "../pages/Product/Product";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+
 const AppRoutes = () => {
     return (
       <>
         <Router>
           <Routes>
             
-            <Route path="/" element={<ProductList />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Product />} />
             <Route path="/order" element={<OrderList />} />
+            <Route path="/login" element={<Login />} />
             
           </Routes>
         </Router>
