ea1f7f6c690c8658c8d166f90a5e612e9c70da80
Iresh Eranga <iresheranga2020al@gmail.com>
2024-07-19 18:39:48 +0530

    Update app-routes.js

--- frontend/src/routes/app-routes.js
+++ frontend/src/routes/app-routes.js
@@ -4,17 +4,26 @@ import OrderList from '../components/OrderList';
 import { Product } from "../pages/Product/Product";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import { ToastContainer } from 'react-toastify';
+import SystemAdminHome from "../pages/SystemAdmin/SystemAdminHome";
+import OutletAdminHome from "../pages/OutletAdmin/OutletAdminHome";
+import SysProducts from "../pages/SystemAdmin/SysProducts";
+
 
 const AppRoutes = () => {
     return (
       <>
         <Router>
+          <ToastContainer/>
           <Routes>
             
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Product />} />
             <Route path="/order" element={<OrderList />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/systemadmin" element={<SystemAdminHome />} />
+            <Route path="/outletadmin" element={<OutletAdminHome />} />
+            <Route path="/systemadmin/products" element={<SysProducts />} />
             
           </Routes>
         </Router>
