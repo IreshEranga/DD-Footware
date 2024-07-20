b9bbf3a44630866d3de46fa6529443be4db6f128
Iresh Eranga <iresheranga2020al@gmail.com>
2024-07-20 16:50:33 +0530

    Update index.js
    add cart Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-
+import { CartProvider } from './context/CartContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CartProvider>
     <App />
+    </CartProvider>
   </React.StrictMode>
 );
 
